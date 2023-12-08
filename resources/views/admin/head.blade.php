ebb02f0ad3ffd375c5bf83daa0fe11cff4edebb1
kittiwatboat <86706288+kittiwatboat@users.noreply.github.com>
2023-12-08 11:13:52 +0700

    แก้ไข title (boat)

--- resources/views/admin/head.blade.php
+++ resources/views/admin/head.blade.php
@@ -4,6 +4,6 @@
 <meta name="description" content="Midone admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
 <meta name="keywords" content="admin template, Midone Admin Template, dashboard template, flat admin template, responsive admin template, web app">
 <meta name="author" content="LEFT4CODE">
-<title>AMSCO</title>
+<title>TCMT Salary calculation system</title>
 <!-- BEGIN: CSS Assets-->
 <link rel="stylesheet" href="{{asset('dist/css/app.css')}}" />
